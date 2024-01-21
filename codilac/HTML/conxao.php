85ccfc95e65efaa3ec5fcc056bbaa22b0466a214
Gouveia Gaspar <gouveiagaspar51@gmail.com>
2024-01-21 23:35:43 +0100

    arquivo de conexão

---
+++ codilac/HTML/conxao.php
@@ -0,0 +1,10 @@
+<?php
+  $servidor = "localhost"; //nome do servidor//
+  $usuario = "root"; //nome do usuário//
+  $senha = ""; //a senhaque é vazia//
+  $BD_codilac = "bd_codilac"; //aqui vai nome do banco de dados que criarmos
+
+
+ $conexao = mysqli_connect($servidor,$usuario,$senha,$BD_banco);
+
+//  primeiro testa a conexão se esta a funcionar e depois podes tentar mandar dados ao banco de dados
