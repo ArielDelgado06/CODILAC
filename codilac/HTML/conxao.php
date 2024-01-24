099ebd5f07a2ba03cc5f231409f7a7dd0e6fd57b
Gouveia Gaspar <gouveiagaspar51@gmail.com>
2024-01-24 23:07:18 +0100

    codigo de conexao

--- codilac/HTML/conxao.php
+++ codilac/HTML/conxao.php
@@ -4,8 +4,34 @@
   $senha = ""; //a senhaque é vazia//
   $BD_codilac = "bd_codilac"; //aqui vai nome do banco de dados que criarmos
 
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $telefone = $_POST['telefone'];
+  $senha = $_POST['senha'];
+  $conf_senha = $_POST['conf_senha'];
+
+  echo($nome);
+  echo($email);
+  echo($telefone);
+  echo($senha);
+  echo($conf_senha);
+
+
 
  $conexao = mysqli_connect($servidor,$usuario,$senha,$BD_banco);
 
 //  primeiro testa a conexão se esta a funcionar e depois podes tentar mandar dados ao banco de dados
+
+if($conexao){
+   
+  $sql = "INSERT INTO cadastros (nome,email,telefone,senha,conf_senha)
+  Values('{$nome}','{$email}','{$telefone}','{$senha}','{$conf_senha}'";
+
+  $inserir = mysqli_query($conexao,$sql);
+
+  if($inserir){
+    echo("cadastro realizado!");
+  }
+  
+}
 
