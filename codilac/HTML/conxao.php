48ab119cec9cb8db4b5a09afe1966a8bf9c47133
Gouveia Gaspar <gouveiagaspar51@gmail.com>
2024-01-25 08:40:17 +0100

    diretório de conexão feito

--- codilac/HTML/conxao.php
+++ codilac/HTML/conxao.php
@@ -1,37 +1,31 @@
 <?php
-  $servidor = "localhost"; //nome do servidor//
-  $usuario = "root"; //nome do usuário//
-  $senha = ""; //a senhaque é vazia//
-  $BD_codilac = "bd_codilac"; //aqui vai nome do banco de dados que criarmos
-
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $telefone = $_POST['telefone'];
-  $senha = $_POST['senha'];
-  $conf_senha = $_POST['conf_senha'];
-
-  echo($nome);
-  echo($email);
-  echo($telefone);
-  echo($senha);
-  echo($conf_senha);
-
-
-
- $conexao = mysqli_connect($servidor,$usuario,$senha,$BD_banco);
-
-//  primeiro testa a conexão se esta a funcionar e depois podes tentar mandar dados ao banco de dados
-
-if($conexao){
-   
-  $sql = "INSERT INTO cadastros (nome,email,telefone,senha,conf_senha)
-  Values('{$nome}','{$email}','{$telefone}','{$senha}','{$conf_senha}'";
-
-  $inserir = mysqli_query($conexao,$sql);
-
-  if($inserir){
-    echo("cadastro realizado!");
-  }
+   $servidor = "localhost"; //nome do servidor//
+   $usuario = "root"; //nome do usuário//
+   $password = ""; //a senha é vazia//
+   $bd_codilac = "codilac"; //aqui vai nome do banco de dados que criamos
+ 
+   // nome dos campos que constam no nosso formulário
+   $nome = $_POST['nome'];
+   $email = $_POST['email'];
+   $telefone = $_POST['telefone'];
+   $senha = $_POST['senha'];
+   $conf_senha = $_POST['conf_senha'];
+ 
+   // codigo de conexão com o banco de dados
+  $conexao = mysqli_connect($servidor,$usuario,$password,$bd_codilac);
   
-}
-
+ 
+ //  codigo para enviar as informações no banco de dados
+ if($conexao){
+    
+   $sql = "INSERT INTO cadastros(nome,email,telefone,senha,conf_senha)
+   Values('{$nome}','{$email}','{$telefone}','{$senha}','{$conf_senha}')";
+ 
+   $inserir = mysqli_query($conexao,$sql);
+ 
+   if($inserir){
+     echo("cadastro realizado!");
+   }
+ }
+ 
+ 
