de6b74ed64c6f3acfb1849bf73c7299298d29ed1
Gouveia Gaspar <gouveiagaspar51@gmail.com>
2024-01-30 15:45:10 +0100

    regras de conexão com o arquivo php

--- codilac/HTML/login.php
+++ codilac/HTML/login.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -5,52 +6,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Tela de Login</title>
+    <title>Tema de Login</title>
     <link rel="stylesheet" href="../CSS/login.css">
 </head>
 
 <body>
-    <Div class="box">
+    <div class="box">
         <div class="img-box">
             <img src="../img/logo_cadilac.jpeg" alt=" " width="450" height="370">
-            
         </div>
         <div class="form-box">
             <h2>Login</h2>
-            
-            <form action="#">
+            <form action="arquivo.php" method="POST">
                 <div class="input-group">
                     <label for="nome">Nome de usuário</label>
-                    <input type="text" id="nome" placeholder="Digite o seu nome de usuário" required>
-
+                    <input type="text" name="nome" id="nome" placeholder="Digite o seu nome de usuário" required>
                 </div>
                 <div class="input-group">
                     <label for="senha">Senha</label>
-                    <input type="password" id="senha" placeholder="Digite a sua senha" required>
-
+                    <input type="password" id="senha" name="senha" placeholder="Digite a sua senha" required>
                 </div>
-                <p>Ainda não está cadastrado? <a href="cadastro.html">Cadastre-se</a></p>
-                
+                <p>Ainda não está cadastrado? <a href="cadastro.php">Cadastre-se</a></p>
                 <div class="input-group">
-                    <button type="submit">LOGIN</button>
+                    <input id="botao" name="submit" type="submit" value="Enviar">
                 </div>
-                
-
-                
-
             </form>
-
         </div>
-
-
-    </Div>
-    
-    
-    
-
-
-
+    </div>
 </body>
-
-
 </html>
