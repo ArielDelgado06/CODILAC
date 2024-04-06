8ac0860191784a0b8aceb58c9c9965145ab77725
Gouveia Gaspar <gouveiagaspar51@gmail.com>
2024-04-06 18:23:55 +0100

    adionar eventos nos botões de marcação

--- codilac/js/script.js
+++ codilac/js/script.js
@@ -17,3 +17,31 @@ toggle.onclick = () =>{
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 }
+
+// cards de marcação de consulta
+const button_one = document.querySelector("#button-one");
+const main_conteiner = document.querySelector(".main");
+
+const event_button = () =>{
+  const box = document.createElement('div');
+  box.classList.add('box');
+
+  const titleContent = document.createElement('h2');
+  titleContent.innerText = 'Especialista';
+
+  const box_especialista = document.createElement('div');
+  box_especialista.classList.add("boxEspecialista");
+
+  const box_img = document.createElement('img');
+  const subTitleContent = document.createElement('h3');
+  subTitleContent.innerText = "Dr. António Gaspar";
+
+  box.appendChild(titleContent);
+  box.appendChild(box_especialista);
+
+  box_especialista.appendChild(box_img);
+  box_especialista.appendChild(subTitleContent);
+  main_conteiner.appendChild(box);
+}
+
+button_one.addEventListener("click", () => event_button());
