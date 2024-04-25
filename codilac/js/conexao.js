c765103d1dabdb105cce36609327ff5f524df4b9
Gouveia Gaspar <gouveiagaspar51@gmail.com>
2024-04-25 13:17:37 +0100

    Arquivos js

---
+++ codilac/js/conexao.js
@@ -0,0 +1,15 @@
+// const url = "localhost:3333/login";
+
+// async function conect() {
+//   try {
+//     const response = await fetch(url);
+
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// }
+
+
+// conect();
---
+++ codilac/js/index.js
@@ -0,0 +1,8 @@
+// const url = "localhost:3000";
+
+// async function connect(){
+//   const response = await fetch("localhost:3333");
+//   console.log(response);
+// }
+
+// connect();
