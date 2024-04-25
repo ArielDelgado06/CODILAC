73f547be32c102d6cedf8dd21ab33ccb516ec14c
WalanyCosta <walanybnegro@gmail.com>
2024-04-25 10:07:20 +0100

    criando a conexão do login

---
+++ codilac/js/login.js
@@ -0,0 +1,62 @@
+const form = document.querySelector(".form_content");
+const nomeInput = form.elements['nome']
+const senhaInput = form.elements['senha']
+const buttonSubmit = document.querySelector("button[value=entrar]")
+let load = true
+
+function isLoad() {
+  buttonSubmit.innerHTML = load ? '...' : 'Entrar'
+  load = !load
+}
+
+async function handleLogin(nome, senha) {
+  try {
+    isLoad()
+    const response = await fetch('http://localhost:3333/login', {
+      method: "POST",
+      headers: { "Content-type": "application/json;charset=UTF-8" },
+      body: JSON.stringify({
+        nome,
+        senha
+      })
+    })
+
+    const data = await response.json()
+
+    if (response.status !== 200) {
+      alert(data?.mensagem)
+      return
+    }
+
+    localStorage.setItem('id', data.id)
+    window.location.href = "home.html"
+  } catch (error) {
+    console.log(error)
+  } finally {
+    isLoad()
+  }
+
+}
+
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  const nome = nomeInput.value
+  const senha = senhaInput.value
+
+  if (nome === '') {
+    alert('Campo nome vazio. Por favor preencha o nome!')
+    return
+  }
+
+  if (senha === '') {
+    alert('Campo senha vazio. Por favor preencha o senha!')
+    return
+  }
+
+  if (nome && senha) {
+    await handleLogin(nome, senha)
+    return
+  }
+
+})
